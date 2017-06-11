bf60facdc5f8923fcac1cb0f184112282c7e65b9
Alex Marchant <alexjmarchant@gmail.com>
2017-06-11 17:33:57 -0400

    Fix cors stuff

--- main.go
+++ main.go
@@ -14,7 +14,13 @@ func main() {
   db = database()
   r := router()
   port := ":" + os.Getenv("PORT")
-
   log.Printf("Starting server on port %s", port)
-  log.Fatal(http.ListenAndServe(port, handlers.CORS()(r)))
+
+  // CORS
+  headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+  originsOk := handlers.AllowedOrigins([]string{"*"})
+  methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PATCH", "OPTIONS"})
+
+  // Start server
+  log.Fatal(http.ListenAndServe(port, handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 }
