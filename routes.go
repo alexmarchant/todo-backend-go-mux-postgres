8cbfc7ccf24356f324e25f22342efe13bb832d72
Alex Marchant <alexjmarchant@gmail.com>
2017-06-12 00:48:51 -0400

    Refector routes

--- routes.go
+++ routes.go
@@ -7,12 +7,12 @@ import (
 func router() *mux.Router {
   r := mux.NewRouter()
   r.Methods("OPTIONS").HandlerFunc(OptionsHandler)
-  r.HandleFunc("/", IndexHandler).Methods("GET")
-  r.HandleFunc("/", CreateHandler).Methods("POST")
-  r.HandleFunc("/", DeleteAllHandler).Methods("DELETE")
-	r.HandleFunc("/tasks/{id}", ReadHandler).Methods("GET")
-	r.HandleFunc("/tasks/{id}", UpdateHandler).Methods("PATCH")
-	r.HandleFunc("/tasks/{id}", DeleteHandler).Methods("DELETE")
+  r.Path("/").Methods("GET").HandlerFunc(IndexHandler)
+  r.Path("/").Methods("POST").HandlerFunc(CreateHandler)
+  r.Path("/").Methods("DELETE").HandlerFunc(DeleteAllHandler)
+	r.Path("/tasks/{id}").Methods("GET").HandlerFunc(ReadHandler)
+	r.Path("/tasks/{id}").Methods("PATCH").HandlerFunc(UpdateHandler)
+	r.Path("/tasks/{id}").Methods("DELETE").HandlerFunc(DeleteHandler)
   return r
 }
 
