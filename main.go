a3d23d4f4ade111588fb4b1f8faa023b87bd85ff
Alex Marchant <alexjmarchant@gmail.com>
2017-06-11 22:01:53 -0400

    Messing w CORS headings

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func main() {
   log.Printf("Starting server on port %s", port)
 
   // CORS
-  headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
   originsOk := handlers.AllowedOrigins([]string{"*"})
-  methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PATCH", "OPTIONS"})
+  headersOk := handlers.AllowedHeaders([]string{"Origin, X-Requested-With, Content-Type, Accept"})
+  methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PATCH", "OPTIONS", "DELETE"})
 
   // Start server
   log.Fatal(http.ListenAndServe(port, handlers.CORS(originsOk, headersOk, methodsOk)(r)))
